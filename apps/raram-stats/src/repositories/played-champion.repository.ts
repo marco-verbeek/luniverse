63ac50857b88e91d2e302cf32460a1c9703125fd
Marco VERBEEK <marcoverbeek99@gmail.com>
2022-12-17 13:05:20 +0100

    chore: define groupSum pipeline stage once

--- apps/raram-stats/src/repositories/played-champion.repository.ts
+++ apps/raram-stats/src/repositories/played-champion.repository.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { AbstractRepository } from '@luni/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, PipelineStage } from 'mongoose';
 
 import { PlayedChampion } from '../schemas/played-champion.schema';
 
@@ -14,44 +14,46 @@ export class PlayedChampionRepository extends AbstractRepository<PlayedChampion>
     super(playedChampionModel);
   }
 
+  groupSumStats: PipelineStage = {
+    $group: {
+      _id: null,
+      gamesPlayed: { $sum: '$gamesPlayed' },
+      gamesWon: { $sum: '$gamesWon' },
+      pointsWon: { $sum: '$pointsWon' },
+      pointsLost: { $sum: '$pointsLost' },
+      kills: { $sum: '$kills' },
+      deaths: { $sum: '$deaths' },
+      assists: { $sum: '$assists' },
+      doubleKills: { $sum: '$doubleKills' },
+      tripleKills: { $sum: '$tripleKills' },
+      quadraKills: { $sum: '$quadraKills' },
+      pentaKills: { $sum: '$pentaKills' },
+      firstBloodKills: { $sum: '$firstBloodKills' },
+      firstBloodAssists: { $sum: '$firstBloodAssists' },
+      damageDone: { $sum: '$damageDone' },
+      damageTaken: { $sum: '$damageTaken' },
+      healed: { $sum: '$healed' },
+      spell1Casts: { $sum: '$spell1Casts' },
+      spell2Casts: { $sum: '$spell2Casts' },
+      spell3Casts: { $sum: '$spell3Casts' },
+      spell4Casts: { $sum: '$spell4Casts' },
+      champLevel: { $sum: '$champLevel' },
+      timePlayed: { $sum: '$timePlayed' },
+      timeCCingOthers: { $sum: '$timeCCingOthers' },
+      totalTimeSpentDead: { $sum: '$totalTimeSpentDead' },
+      goldEarned: { $sum: '$goldEarned' },
+      goldSpent: { $sum: '$goldSpent' },
+      totalMinionsKilled: { $sum: '$totalMinionsKilled' },
+      itemsPurchased: { $sum: '$itemsPurchased' },
+    },
+  };
+
   async getPlayerSums(discordId: string) {
     return this.model.aggregate([
       {
         $match: { discordId },
       },
-      {
-        $group: {
-          _id: null,
-          gamesPlayed: { $sum: '$gamesPlayed' },
-          gamesWon: { $sum: '$gamesWon' },
-          pointsWon: { $sum: '$pointsWon' },
-          pointsLost: { $sum: '$pointsLost' },
-          kills: { $sum: '$kills' },
-          deaths: { $sum: '$deaths' },
-          assists: { $sum: '$assists' },
-          doubleKills: { $sum: '$doubleKills' },
-          tripleKills: { $sum: '$tripleKills' },
-          quadraKills: { $sum: '$quadraKills' },
-          pentaKills: { $sum: '$pentaKills' },
-          firstBloodKills: { $sum: '$firstBloodKills' },
-          firstBloodAssists: { $sum: '$firstBloodAssists' },
-          damageDone: { $sum: '$damageDone' },
-          damageTaken: { $sum: '$damageTaken' },
-          healed: { $sum: '$healed' },
-          spell1Casts: { $sum: '$spell1Casts' },
-          spell2Casts: { $sum: '$spell2Casts' },
-          spell3Casts: { $sum: '$spell3Casts' },
-          spell4Casts: { $sum: '$spell4Casts' },
-          champLevel: { $sum: '$champLevel' },
-          timePlayed: { $sum: '$timePlayed' },
-          timeCCingOthers: { $sum: '$timeCCingOthers' },
-          totalTimeSpentDead: { $sum: '$totalTimeSpentDead' },
-          goldEarned: { $sum: '$goldEarned' },
-          goldSpent: { $sum: '$goldSpent' },
-          totalMinionsKilled: { $sum: '$totalMinionsKilled' },
-          itemsPurchased: { $sum: '$itemsPurchased' },
-        },
-      },
+      this.groupSumStats,
       {
         $project: {
           _id: 0,
@@ -65,39 +67,7 @@ export class PlayedChampionRepository extends AbstractRepository<PlayedChampion>
       {
         $match: { championId },
       },
-      {
-        $group: {
-          _id: null,
-          gamesPlayed: { $sum: '$gamesPlayed' },
-          gamesWon: { $sum: '$gamesWon' },
-          pointsWon: { $sum: '$pointsWon' },
-          pointsLost: { $sum: '$pointsLost' },
-          kills: { $sum: '$kills' },
-          deaths: { $sum: '$deaths' },
-          assists: { $sum: '$assists' },
-          doubleKills: { $sum: '$doubleKills' },
-          tripleKills: { $sum: '$tripleKills' },
-          quadraKills: { $sum: '$quadraKills' },
-          pentaKills: { $sum: '$pentaKills' },
-          firstBloodKills: { $sum: '$firstBloodKills' },
-          firstBloodAssists: { $sum: '$firstBloodAssists' },
-          damageDone: { $sum: '$damageDone' },
-          damageTaken: { $sum: '$damageTaken' },
-          healed: { $sum: '$healed' },
-          spell1Casts: { $sum: '$spell1Casts' },
-          spell2Casts: { $sum: '$spell2Casts' },
-          spell3Casts: { $sum: '$spell3Casts' },
-          spell4Casts: { $sum: '$spell4Casts' },
-          champLevel: { $sum: '$champLevel' },
-          timePlayed: { $sum: '$timePlayed' },
-          timeCCingOthers: { $sum: '$timeCCingOthers' },
-          totalTimeSpentDead: { $sum: '$totalTimeSpentDead' },
-          goldEarned: { $sum: '$goldEarned' },
-          goldSpent: { $sum: '$goldSpent' },
-          totalMinionsKilled: { $sum: '$totalMinionsKilled' },
-          itemsPurchased: { $sum: '$itemsPurchased' },
-        },
-      },
+      this.groupSumStats,
       {
         $project: {
           _id: 0,
