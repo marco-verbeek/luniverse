3da1535ad0c4ebd9ffa8b6e3374798196159dbe0
Marco <56871713+marco-verbeek@users.noreply.github.com>
2022-12-29 10:48:40 +0100

    chore(auth): remove auth v1 (#36)

--- apps/auth/src/dtos/confirm-league-link.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsString, Length } from 'class-validator';
-
-export class ConfirmLeagueLinkDTO {
-  @IsString()
-  @Length(18, 18)
-  accountId: string;
-}
--- apps/auth/src/dtos/get-user-by-summoner-name.dto.ts
+++
@@ -1,6 +0,0 @@
-import { IsString } from 'class-validator';
-
-export class DiscordIdBySummonerNameDTO {
-  @IsString({ each: true })
-  summonerNames: string[];
-}
