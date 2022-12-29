f7cd7f7391918a5d46ab933cda67cf7aa4b183b2
Marco VERBEEK <marcoverbeek99@gmail.com>
2022-12-29 10:47:50 +0100

    chore(auth): remove auth v1

--- apps/auth/src/decorators/get-discord-user-id.decorator.ts
+++
@@ -1,8 +0,0 @@
-import { ExecutionContext, createParamDecorator } from '@nestjs/common';
-
-export const GetDiscordUserId = createParamDecorator(
-  (data: undefined, ctx: ExecutionContext): number => {
-    const request = ctx.switchToHttp().getRequest();
-    return request.user['discordId'];
-  },
-);
