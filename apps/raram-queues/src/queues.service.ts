83db8bcd3097bc634f92b5c7a4f3a36c8c0941cd
Marco VERBEEK <marcoverbeek99@gmail.com>
2022-12-30 01:02:13 +0100

    wip: funcs prints what they will do

--- apps/raram-queues/src/queues.service.ts
+++ apps/raram-queues/src/queues.service.ts
@@ -3,10 +3,21 @@ import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
 export class QueuesService {
-  // TODO: shared array of users in a game. Allows for easier scalability with multiple microservices.
+  // TODO: shared array of users in a game. Allows for easier scalability with multiple microservices?
+  // TODO: or even better, rework this when needed. We might not have to scale this if we keep it between friends!
+
+  // TODO: logger for easier debugging.
 
   @Cron(CronExpression.EVERY_5_MINUTES)
   inQueueHandle() {
-    console.log('what to do with queuing players');
+    console.log('skip player if already in active game');
+    console.log('check if players are in an active game');
+    console.log('if they are, add them to a shared array of in-game players');
+  }
+
+  @Cron(CronExpression.EVERY_30_SECONDS)
+  inGameHandle() {
+    console.log('check if player still in game');
+    console.log('if not, analyze latest game');
   }
 }
