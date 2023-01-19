1e2237a0cff673479d073f9e48ce4e817e50a7ed
Marco VERBEEK <marcoverbeek99@gmail.com>
2023-01-19 18:27:09 +0100

    feat: improve scraper and implement bundler

--- apps/snowd-api/scraper/src/scraper.js
+++ apps/snowd-api/scraper/src/scraper.js
@@ -8,22 +8,44 @@ const alphabet =
 const customNanoId = customAlphabet(alphabet, 21);
 
 const normalizeChampName = (championName) => {
-  if (!championName.includes('_')) {
-    return (
-      championName.charAt(0).toUpperCase() + championName.slice(1).toLowerCase()
-    );
+  // Unique cases, most of these don't work... TODO: fix
+  switch (championName) {
+    case Champions.DR_MUNDO:
+      return 'Dr._Mundo';
+    case Champions.JARVAN_IV:
+      return 'Jarvan_IV';
+    case Champions.AURELION_SOL:
+      return 'Aurelion_Sol';
+    case Champions.KAISA:
+      return 'Kai%27Sa';
+    case Champions.CHOGATH:
+      return 'Cho%27Gath';
+    case Champions.KHAZIX:
+      return 'Kha%27Zix';
+    case Champions.KOG_MAW:
+      return 'Kog%27Maw';
+    case Champions.LE_BLANC:
+      return 'LeBlanc';
+    case Champions.TRISTANA:
+      return 'Tristana';
+    case Champions.VELKOZ:
+      return 'Vel%27Koz';
   }
 
-  let [a, b] = championName.split('_');
-  a = a.charAt(0).toUpperCase() + a.slice(1).toLowerCase();
-  b = b.charAt(0).toUpperCase() + b.slice(1).toLowerCase();
+  if (championName.includes('_')) {
+    let [a, b] = championName.split('_');
+    a = a.charAt(0).toUpperCase() + a.slice(1).toLowerCase();
+    b = b.charAt(0).toUpperCase() + b.slice(1).toLowerCase();
 
-  return `${a}_${b}`;
+    return `${a}_${b}`;
+  }
+
+  return (
+    championName.charAt(0).toUpperCase() + championName.slice(1).toLowerCase()
+  );
 };
 
-const getAudioData = async (browser, championName) => {
-  const page = await browser.newPage();
-
+const getAudioData = async (page, championName) => {
   await page.goto(
     `https://leagueoflegends.fandom.com/wiki/${normalizeChampName(
       championName,
@@ -59,25 +81,25 @@ const getAudioData = async (browser, championName) => {
 
 (async () => {
   const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
   for (const championName of Object.keys(Champions)) {
-    // TODO: fix, not working
-    if (fs.existsSync('./data/audio_data_' + championName)) {
-      console.log(`${championName} already scraped. Skipping!`);
+    if (fs.existsSync(`./data/audio_data_${championName}.json`)) {
+      console.log(`Skipping: ${championName}`);
       continue;
     }
 
-    const audioData = await getAudioData(browser, championName);
+    const audioData = await getAudioData(page, championName);
 
     fs.writeFileSync(
       `./data/audio_data_${championName}.json`,
       JSON.stringify([...audioData], null, 2),
     );
 
-    console.log(`${championName} audio data saved!`);
+    console.log(`Saved: ${championName}`);
   }
 
-  console.log('All champion data saved.');
+  console.log("Done: all champions' data saved.");
 
   await browser.close();
 })();
